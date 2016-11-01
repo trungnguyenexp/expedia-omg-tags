b67cc5a0ea19b7b44e47334a232c117a19c7ddd0
Tony Lagnada <anlagnada@expedia.com>
2016-11-01 10:31:01 -0700

    Custom tags from main profile - sync'd with tealium.

---
+++ src/profile/main/tag/TDEST-Pixel.js
@@ -0,0 +1,133 @@
+//~~tv:20010.20140827
+//~~tc: Tealium Custom Container
+
+/*
+ Tealium Custom Container Notes:
+ - Add sending code between "Start Tag Sending Code" and "End Tag Sending Code".
+ - Add JavaScript tag library code between "Start Tag Library Code" and "End Tag Library Code".
+ - Add JavaScript code only, do not add HTML code in this file.
+ - Remove any <script> and </script> tags from the code you place in this file.
+
+ Loading external JavaScript files (Loader):
+ - If you need to load an additional external JavaScript file, un-comment the singe-line JavaScript comments ("//") within the following Loader sections near the bottom of this file:
+ - "Start Loader Function Call"
+ - "End Loader Function Call"
+ - "Start Loader Callback Function"
+ - "End Loader Callback Function"
+ - After un-commenting, insert the path to the external JavaScript file you want to load.
+ - Finally, within the Loader callback function, insert the JavaScript code that should run after the external JavaScript file has loaded.
+ */
+
+/* Start Tag Library Code */
+/* End Tag Library Code */
+
+//tealium universal tag - utag.sender.custom_container ut4.0.##UTVERSION##, Copyright ##UTYEAR## Tealium.com Inc. All Rights Reserved.
+try {
+    (function (id, loader) {
+        var u = {};
+        utag.o[loader].sender[id] = u;
+
+        // Start Tealium loader 4.32
+        // Please do not modify
+        if (utag === undefined) { utag = {}; } if (utag.ut === undefined) { utag.ut = {}; } if (utag.ut.loader === undefined) { u.loader = function (o) { var a, b, c, l; a = document; if (o.type === "iframe") { b = a.createElement("iframe"); b.setAttribute("height", "1"); b.setAttribute("width", "1"); b.setAttribute("style", "display:none"); b.setAttribute("src", o.src); } else if (o.type === "img") { utag.DB("Attach img: " + o.src); b = new Image(); b.src = o.src; return; } else { b = a.createElement("script"); b.language = "javascript"; b.type = "text/javascript"; b.async = 1; b.charset = "utf-8"; b.src = o.src; } if (o.id) { b.id = o.id; } if (typeof o.cb === "function") { if (b.addEventListener) { b.addEventListener("load", function () { o.cb(); }, false); } else { b.onreadystatechange = function () { if (this.readyState === "complete" || this.readyState === "loaded") { this.onreadystatechange = null; o.cb(); } }; } } l = o.loc || "head"; c = a.getElementsByTagName(l)[0]; if (c) { utag.DB("Attach to " + l + ": " + o.src); if (l === "script") { c.parentNode.insertBefore(b, c); } else { c.appendChild(b); } } }; } else { u.loader = utag.ut.loader; }
+        // End Tealium loader
+
+        u.ev = {'view' : 1};
+
+        u.initialized = false;
+
+    ##UTGEN##
+
+    u.send = function(a, b) {
+        if (u.ev[a] || u.ev.all !== undefined) {
+            //##UTENABLEDEBUG##utag.DB("send:##UTID##");
+
+            var c, d, e, f, i;
+
+            u.data = {
+                /* Initialize default tag parameter values here */
+                /* Examples: */
+                "dest" : "",
+                "checkInDate" : "",
+                "checkOutDate" : "",
+                "tdest_random" : "",
+                "base_url" : "/ads/tripdest?dest="
+                /* A value mapped to "account_id" or "base_url" in TiQ will replace these default values. */
+            };
+
+
+            /* Start Tag-Scoped Extensions Code */
+            /* Please Do Not Edit This Section */
+        ##UTEXTEND##
+            /* End Tag-Scoped Extensions Code */
+
+
+            /* Start Mapping Code */
+            for (d in utag.loader.GV(u.map)) {
+                if (b[d] !== undefined && b[d] !== "") {
+                    e = u.map[d].split(",");
+                    for (f = 0; f < e.length; f++) {
+                        u.data[e[f]] = b[d];
+                    }
+                }
+            }
+            /* End Mapping Code */
+
+
+            /* Start Tag Sending Code */
+
+            var axel = Math.random() + "";
+            u.data.tdest_random = axel * 10000000000000;
+            if(utag_data && utag_data.dest && utag_data.checkInDate && utag_data.checkOutDate) {
+                u.data.dest = utag_data.dest || "";
+                window.convertDate = function(date) {
+                    return date.substring(5,7)+'/'+date.substring(8,10)+'/'+date.substring(2,4);
+                };
+            }
+
+            u.data.checkInDate = typeof utag_data.checkInDate != "undefined" && utag_data.checkInDate != "" ? convertDate(utag_data.checkInDate) : "";
+            u.data.checkOutDate = typeof utag_data.checkOutDate != "undefined" && utag_data.checkOutDate != "" ? convertDate(utag_data.checkInDate) : "";
+
+            /* End Tag Sending Code */
+
+
+            /* Start Loader Callback Function */
+            /* Un-comment the single-line JavaScript comments ("//") to use this Loader callback function. */
+
+            //u.loader_cb = function () {
+            //u.initialized = true;
+            /* Start Loader Callback Tag Sending Code */
+
+            // Insert your post-Loader tag sending code here.
+
+            /* End Loader Callback Tag Sending Code */
+            //};
+
+            /* End Loader Callback Function */
+
+
+            /* Start Loader Function Call */
+            /* Un-comment the single-line JavaScript comments ("//") to use Loader. */
+
+            //if (!u.initialized) {
+            //u.loader({"type" : "iframe", "src" : u.data.base_url + c.join(u.data.qsp_delim), "cb" : u.loader_cb, "loc" : "body", "id" : 'utag_##UTID##' });
+            //u.loader({"type" : "script", "src" : u.data.base_url, "cb" : u.loader_cb, "loc" : "script", "id" : 'utag_##UTID##' });
+            //} else {
+            //u.loader_cb();
+            //}
+
+            u.loader({"type" : "img", "src" : u.data.base_url + u.data.dest + '&startDate='+u.data.checkInDate+'&endDate='+u.data.checkOutDate+'&num=' + u.data.tdest_random +'?', "id" : 'tripDest' });
+
+            /* End Loader Function Call */
+
+
+            //##UTENABLEDEBUG##utag.DB("send:##UTID##:COMPLETE");
+        }
+    };
+        utag.o[loader].loader.LOAD(id);
+    })("##UTID##", "##UTLOADERID##");
+} catch (error) {
+    utag.DB(error);
+}
+//end tealium universal tag
+
