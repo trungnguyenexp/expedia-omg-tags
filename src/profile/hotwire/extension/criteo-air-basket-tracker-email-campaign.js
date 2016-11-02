aaf58c81a43c79434db95c9c69561cc62e44e714
Tony Lagnada <anlagnada@expedia.com>
2016-11-02 16:29:51 -0700

    Sync current version from Tealium

---
+++ src/profile/hotwire/extension/criteo-air-basket-tracker-email-campaign.js
@@ -0,0 +1,8 @@
+window.criteo_q = window.criteo_q || [];
+window.criteo_q.push( {
+        event: "setAccount", account: ""},{
+        event: "setHashedEmail", email: "" },{
+        event: "setSiteType", type: b["device_type_abbreviated"] },
+    { event: "viewBasket" ,
+        item: [ { id: b["Tag_Criteo_Air_Airport_Codes_Checkout"], price: b["Tag_Criteo_Flight_Checkout_PricePerSeat"], quantity: b["flight_checkoutPage_numberOfSeats"]}
+        ]});
---
+++ src/profile/hotwire/extension/criteo-air-transaction-tracker-email-campaign.js
@@ -0,0 +1,37 @@
+if(typeof utag_data['entity.checkout.flightOffer.flight.legs.0'] != "undefined") {
+    utag_data['entity.checkout.flightOffer.flight.departureDate'] = utag_data['entity.checkout.flightOffer.flight.legs.0.isoFormatDepartureTimestamp'];
+    var count = 1;
+    while(true) {
+        var arrivalDate = 'entity.checkout.flightOffer.flight.legs.'+count+'.isoFormatArrivalTimestamp';
+        if(typeof utag_data[arrivalDate] == "undefined") {
+            break;
+        }
+        count++;
+    }
+    var count = count - 1;
+    var arrivalDate = 'entity.checkout.flightOffer.flight.legs.'+count+'.isoFormatArrivalTimestamp';
+    utag_data['entity.checkout.flightOffer.flight.arrivalDate'] = utag_data[arrivalDate]
+}
+if (typeof utag_data['entity.checkout.flightOffer.flight.legs.0.isoFormatDepartureTimestamp'] !== 'undefined' && typeof utag_data['entity.checkout.flightOffer.flight.arrivalDate'] !== 'undefined')
+{
+    var splitDeparture = utag_data['entity.checkout.flightOffer.flight.legs.0.isoFormatDepartureTimestamp'].slice(0, 10).split('-');
+    utag_data['entity.checkout.flightOffer.flight.departureDate_MMDDYYYY'] = splitDeparture[1] +'/'+ splitDeparture[2] +'/'+ splitDeparture[0];
+    var splitArrival = utag_data['entity.checkout.flightOffer.flight.arrivalDate'].slice(0, 10).split('-');
+    utag_data['entity.checkout.flightOffer.flight.arrivalDate_MMDDYYYY'] = splitArrival[1] +'/'+ splitArrival[2] +'/'+ splitArrival[0];
+}
+window.criteo_q = window.criteo_q || [];window.criteo_q.push
+( { event: "manualFlushing"},
+    { event: "setAccount", account: [19574,19575] },{ event: "manualDising"},
+    { event: "setHashedEmail", email: "" },
+    { event: "setSiteType", type: b["device_type_abbreviated"] },
+    { event: "viewSearch", checkin_date: b["entity.checkout.flightOffer.flight.departureDate_MMDDYYYY"], checkout_date: b["entity.checkout.flightOffer.flight.arrivalDate_MMDDYYYY"]},
+    { event: "setData", ui_comingFrom: "flightconfirm", ui_destcity: b["flight_confirmationPage_destinationAirportCity"], ui_deststate: b["flight_confirmationPage_destinationAirportState"]},
+    { event: "flushEvents"},
+    { event: "setAccount", account: 19576 },
+    { event: "setHashedEmail", email: "" },
+    { event: "setSiteType", type: b["device_type_abbreviated"] },
+    { event: "trackTransaction" , id: b.entity.checkout.itineraryNumber,
+        new_customer: "",
+        item: [ { id: b.entity.checkout.flightOffer.originAirportCode+ "/" + b.entity.checkout.flightOffer.destinationAirportCode, price: b["pricePerPerson"], quantity: b["totalAirTicketQuantity"]}]},
+    { event: "flushEvents"}
+);
