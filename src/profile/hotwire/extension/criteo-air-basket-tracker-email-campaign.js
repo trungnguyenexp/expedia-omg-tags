df4d1d8728497c4058a3de8946ac784c4c09125e
Tony Lagnada <anlagnada@expedia.com>
2016-11-02 16:48:20 -0700

    Hotwire criteo change request

--- src/profile/hotwire/extension/criteo-air-basket-tracker-email-campaign.js
+++ src/profile/hotwire/extension/criteo-air-basket-tracker-email-campaign.js
@@ -2,6 +2,7 @@ window.criteo_q = window.criteo_q || [];
 window.criteo_q.push( {
         event: "setAccount", account: ""},{
         event: "setHashedEmail", email: "" },{
+        event: "setData", splitGroup: b.cp.vt_PIX16},{
         event: "setSiteType", type: b["device_type_abbreviated"] },
     { event: "viewBasket" ,
         item: [ { id: b["Tag_Criteo_Air_Airport_Codes_Checkout"], price: b["Tag_Criteo_Flight_Checkout_PricePerSeat"], quantity: b["flight_checkoutPage_numberOfSeats"]}
