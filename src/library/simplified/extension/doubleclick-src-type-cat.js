d0635c9027d6ff2a94608870387e09b9a45aeac5
yonaik <v-ynaik@expedia.com>
2017-01-31 19:21:35 +0530

    Updating doubleclick extension 9729,9730 and 9731 for src/cat/type values

--- src/library/simplified/extension/doubleclick-src-type-cat.js
+++ src/library/simplified/extension/doubleclick-src-type-cat.js
@@ -166,6 +166,41 @@ try {
                 b['doubleclick.type'] = "emeaa616";
                 b['doubleclick.cat'] = "emeaa001";
                 break;
+            case 1146:
+                b['doubleclick.src'] = "3645995";
+                b['doubleclick.type'] = "nausa900";
+                b['doubleclick.cat'] = "nausa992";
+                break;
+            case 70125:
+                b['doubleclick.src'] = "4598511";
+                b['doubleclick.type'] = "apaca0";
+                b['doubleclick.cat'] = "apaca0";
+                break;
+            case 70129:
+                b['doubleclick.src'] = "4600929";
+                b['doubleclick.type'] = "apacn00";
+                b['doubleclick.cat'] = "apacn0";
+                break;
+            case 70150:
+                b['doubleclick.src'] = "4693435";
+                b['doubleclick.type'] = "apaca00";
+                b['doubleclick.cat'] = "apaca0";
+                break;
+            case 70151:
+                b['doubleclick.src'] = "4692458";
+                b['doubleclick.type'] = "apacn00";
+                b['doubleclick.cat'] = "apacn0";
+                break;
+            case 70201:
+                b['doubleclick.src'] = "5179340";
+                b['doubleclick.type'] = "nauso00";
+                b['doubleclick.cat'] = "orb01";
+                break;
+            case 70301:
+                b['doubleclick.src'] = "5192003";
+                b['doubleclick.type'] = "nausc0";
+                b['doubleclick.cat'] = "ctix01";
+                break;
             default:
                 break;
         }
@@ -174,183 +209,3 @@ try {
 catch (err){
 
 }
-
-/** old
- try {
-    if (utag_data.siteId && (utag.isHomePage() || utag.isHotelLanding() || utag.isFlightLanding() || utag.isCarLanding() || utag.isCruiseLanding() || utag.isPackageLanding() )) {
-        switch (utag_data.siteId) {
-            case 1:
-                utag_data['doubleclick.src'] = "2588783";
-                utag_data['doubleclick.type'] = "nausc547";
-                utag_data['doubleclick.cat'] = "naush134";
-                break;
-            case 3:
-                utag_data['doubleclick.src'] = "2588791";
-                utag_data['doubleclick.type'] = "emeau111";
-                utag_data['doubleclick.cat'] = "emeau642";
-                break;
-            case 4:
-                utag_data['doubleclick.src'] = "2588785";
-                utag_data['doubleclick.type'] = "nacan212";
-                utag_data['doubleclick.cat'] = "nacan728";
-                break;
-            case 6:
-                utag_data['doubleclick.src'] = "2589196";
-                utag_data['doubleclick.type'] = "emeag857";
-                utag_data['doubleclick.cat'] = "emeag810";
-                break;
-            case 8:
-                utag_data['doubleclick.src'] = "2589197";
-                utag_data['doubleclick.type'] = "emeai699";
-                utag_data['doubleclick.cat'] = "emeai243";
-                break;
-            case 9:
-                utag_data['doubleclick.src'] = "2589198";
-                utag_data['doubleclick.type'] = "emeas940";
-                utag_data['doubleclick.cat'] = "emeas528";
-                break;
-            case 11:
-                utag_data['doubleclick.src'] = "2589205";
-                utag_data['doubleclick.type'] = "emean493";
-                utag_data['doubleclick.cat'] = "emean350";
-                break;
-            case 12:
-                utag_data['doubleclick.src'] = "2589193";
-                utag_data['doubleclick.type'] = "namex113";
-                utag_data['doubleclick.cat'] = "namex753";
-                break;
-            case 14:
-                utag_data['doubleclick.src'] = "2945316";
-                utag_data['doubleclick.type'] = "apacs971";
-                utag_data['doubleclick.cat'] = "apacs058";
-                break;
-            case 15:
-                utag_data['doubleclick.src'] = "2997500";
-                utag_data['doubleclick.type'] = "apacm103";
-                utag_data['doubleclick.cat'] = "apacm950";
-                break;
-            case 16:
-                utag_data['doubleclick.src'] = "2997587";
-                utag_data['doubleclick.type'] = "apack568";
-                utag_data['doubleclick.cat'] = "apack492";
-                break;
-            case 17:
-                utag_data['doubleclick.src'] = "2997550";
-                utag_data['doubleclick.type'] = "apact438";
-                utag_data['doubleclick.cat'] = "apact016";
-                break;
-            case 18:
-                utag_data['doubleclick.src'] = "3012675";
-                utag_data['doubleclick.type'] = "apach256";
-                utag_data['doubleclick.cat'] = "apach734";
-                break;
-            case 20:
-                utag_data['doubleclick.src'] = "2589195";
-                utag_data['doubleclick.type'] = "emeaf419";
-                utag_data['doubleclick.cat'] = "emeaf590";
-                break;
-            case 25:
-                utag_data['doubleclick.src'] = "2589206";
-                utag_data['doubleclick.type'] = "apaca499";
-                utag_data['doubleclick.cat'] = "apaca551";
-                break;
-            case 27:
-                utag_data['doubleclick.src'] = "2589208";
-                utag_data['doubleclick.type'] = "apaci060";
-                utag_data['doubleclick.cat'] = "apaci186";
-                break;
-            case 28:
-                utag_data['doubleclick.src'] = "2589209";
-                utag_data['doubleclick.type'] = "apacj240";
-                utag_data['doubleclick.cat'] = "apacj512";
-                break;
-            case 29:
-                utag_data['doubleclick.src'] = "2589207";
-                utag_data['doubleclick.type'] = "apacn043";
-                utag_data['doubleclick.cat'] = "apacn792";
-                break;
-            case 61:
-                utag_data['doubleclick.src'] = "3193419";
-                utag_data['doubleclick.type'] = "apaci419";
-                utag_data['doubleclick.cat'] = "apaci201";
-                break;
-            case 62:
-                utag_data['doubleclick.src'] = "2997604";
-                utag_data['doubleclick.type'] = "apact361";
-                utag_data['doubleclick.cat'] = "apact726";
-                break;
-            case 63:
-                utag_data['doubleclick.src'] = "2589199";
-                utag_data['doubleclick.type'] = "emeai191";
-                utag_data['doubleclick.cat'] = "emeai325";
-                break;
-            case 64:
-                utag_data['doubleclick.src'] = "2589204";
-                utag_data['doubleclick.type'] = "emeab264";
-                utag_data['doubleclick.cat'] = "emeab876";
-                break;
-            case 65:
-                utag_data['doubleclick.src'] = "2589201";
-                utag_data['doubleclick.type'] = "emeas325";
-                utag_data['doubleclick.cat'] = "emeas221";
-                break;
-            case 66:
-                utag_data['doubleclick.src'] = "2589200";
-                utag_data['doubleclick.type'] = "emean433";
-                utag_data['doubleclick.cat'] = "emean062";
-                break;
-            case 67:
-                utag_data['doubleclick.src'] = "2589202";
-                utag_data['doubleclick.type'] = "emead466";
-                utag_data['doubleclick.cat'] = "emead550";
-                break;
-            case 68:
-                utag_data['doubleclick.src'] = "2997741";
-                utag_data['doubleclick.type'] = "apacp656";
-                utag_data['doubleclick.cat'] = "apacp606";
-                break;
-            case 69:
-                utag_data['doubleclick.src'] = "3224677";
-                utag_data['doubleclick.type'] = "latam050";
-                utag_data['doubleclick.cat'] = "latam291";
-                break;
-            case 70:
-                utag_data['doubleclick.src'] = "3291249";
-                utag_data['doubleclick.type'] = "latam881";
-                utag_data['doubleclick.cat'] = "latam953";
-                break;
-            case 71:
-                utag_data['doubleclick.src'] = "3291279";
-                utag_data['doubleclick.type'] = "apacv307";
-                utag_data['doubleclick.cat'] = "apacv218";
-                break;
-            case 72:
-                utag_data['doubleclick.src'] = "3847545";
-                utag_data['doubleclick.type'] = "emeac614";
-                utag_data['doubleclick.cat'] = "emeac349";
-                break;
-            case 73:
-                utag_data['doubleclick.src'] = "3847494";
-                utag_data['doubleclick.type'] = "emeaf270";
-                utag_data['doubleclick.cat'] = "emeaf786";
-                break;
-            case 80001:
-                utag_data['doubleclick.src'] = "5439637";
-                utag_data['doubleclick.type'] = "natra00";
-                utag_data['doubleclick.cat'] = "natra0";
-                break;
-            case 10122006:
-                utag_data['doubleclick.src'] = "2589203";
-                utag_data['doubleclick.type'] = "emeaa616";
-                utag_data['doubleclick.cat'] = "emeaa001";
-                break;
-            default:
-                break;
-        }
-    }
-}
- catch (err){
-
-}
-
- **/
