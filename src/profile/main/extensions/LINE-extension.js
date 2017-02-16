8b9e407d1466696362cb13d7c4ee30ae570bf36c
Trung Nguyen <trunguyen@BELC02R3FSYG8WN.sea.corp.expecn.com>
2017-02-15 20:17:16 -0800

    Update condition for Mobilehotel and Home pages

--- src/profile/main/extensions/LINE-extension.js
+++ src/profile/main/extensions/LINE-extension.js
@@ -1,13 +1,18 @@
 b["lineParameterExists"] = "false";
 if (typeof(window.utag_data) !== "undefined" && window.utag_data["device.device.type"].indexOf("Mobile") > -1 && (document.URL.indexOf("www.expedia.co.jp") > -1 || document.URL.indexOf("wwwexpediacojp") > -1))
- {
+{
     if (document.URL.indexOf("/user/createaccount") > -1 || document.URL.indexOf("/user/signin") > -1)
     {
-        if(('localStorage' in window) && (window.localStorage !== null)) 
+        if (('localStorage' in window) && (window.localStorage !== null))
         {
-          localStorage.removeItem("line_cc");
+            var paramsObj = getParamsObj();
+            var ls_cc = paramsObj['line_cc'];
+            if (typeof(ls_cc) !== undefined && (localStorage.getItem("line_cc") == null || localStorage.getItem("line_cc") == "undefined" || localStorage.getItem("line_cc") == ""))
+            {
+                localStorage.setItem("line_cc",ls_cc);
+            }
         }
-        
+
         function getParamsObj() {
             var query = location.search.substr(1);
             var params = query.split('&');
@@ -22,17 +27,9 @@ if (typeof(window.utag_data) !== "undefined" && window.utag_data["device.device.
             }
             return paramsObj;
         }
-
-        var paramsObj = getParamsObj();
-        var ls_cc = paramsObj['line_cc'];
-        if (typeof(ls_cc) !== undefined) {
-            if(('localStorage' in window) && (window.localStorage !== null)) {
-              localStorage.setItem("line_cc",ls_cc);
-            }
-        }
     }
 
-    if (typeof(window.utag_data) !== "undefined" && ((typeof(window.utag_data["pageName"]) !== "undefined" && window.utag_data["pageName"].indexOf("page.mobilehotellandingpage") > -1) || (typeof(window.utag_data["utagPageName"]) !== "undefined" && utag_data["utagPageName"].indexOf("Home") > -1)))
+    if (document.URL.indexOf("/MobileHotel?") > -1 || utag_data["utagPageName"].indexOf("Home") > -1)
     {
         if ((typeof(window.utag_data["userState"]) !== "undefined" && (window.utag_data["userState"].toLowerCase() === "identified" || window.utag_data["userState"].toLowerCase() === "authenticated"))
             || (typeof(window.utag_data["loggedUser"]) !== "undefined" && window.utag_data["loggedUser"].toLowerCase() === "logged user"))
@@ -51,15 +48,20 @@ if (typeof(window.utag_data) !== "undefined" && window.utag_data["device.device.
                 b["line_uid"] = utag_data["context.user.expUserProfileId"];
             }
 
-            if (('localStorage' in window) && (window.localStorage !== null)) 
+            if (('localStorage' in window) && (window.localStorage !== null))
             {
                 b["line_cc"] = localStorage.getItem("line_cc");
 
-                if (typeof(b["line_cc"])!== "undefined" && b["line_cc"]!=="" && typeof(b["line_uid"])!=="undefined" && b["line_uid"] !== "") 
+                if (typeof(b["line_cc"])!== "undefined" && b["line_cc"]!=="" && typeof(b["line_uid"])!=="undefined" && b["line_uid"] !== "")
                 {
                     b["lineParameterExists"] = "true";
                 }
             }
+        }
+
+        if(('localStorage' in window) && (window.localStorage !== null))
+        {
+            localStorage.removeItem("line_cc");
         }
     }
 }
