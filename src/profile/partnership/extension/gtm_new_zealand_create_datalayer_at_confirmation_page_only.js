d50a71e6c95434a32a767923f93f05d21d13d904
Trung Nguyen <trunguyen@BELC02R3FSYG8WN.sea.corp.expecn.com>
2017-03-16 17:48:11 -0700

    minor update from code review

--- src/profile/partnership/extension/gtm_new_zealand_create_datalayer_at_confirmation_page_only.js
+++ src/profile/partnership/extension/gtm_new_zealand_create_datalayer_at_confirmation_page_only.js
@@ -18,14 +18,14 @@ if (typeof b.utagPageName != "undefined" && b.utagPageName.toLowerCase().indexOf
                     'name': b['hotelName'].toLowerCase(), //string - name of the product - use propertytype?
                     'price': b['checkout.cartTotal.amount'], //float - should be in storefront currency value
                     'brand': b['SiteBrand'].toLowerCase(), //string
-                    'category': 'ancillary/hotels/' + b['starRating'] + '/' +  b['hotelType'].toLowerCase(), //string - set the levels of category separated by "/" - first level "Ancillary", next level "hotels", then <star rating>, followed by <property type> (if available)
+                    'category': 'ancillary/hotels/' + b['starRating'], //string - set the levels of category separated by "/" - first level "Ancillary", next level "hotels", then <star rating>, followed by <property type> (if available)
                     'variant': 'hotels', //string
                     'quantity': b['numberOfRooms'], //integer - number of rooms booked
                     'dimension35': b['lengthOfStay'], //integer - trip duration
                     'dimension33': b['checkInDate'], //string - check in date
                     'dimension37': b['bookingWindow'], //integer - lead days assuming purchase was made on 16 jan 2017
                     'dimension38': b['numberOfGuests'], //integer - total number of guests in booking
-                    'dimension53': b['city'].toLowerCase() + b['country'].toLowerCase(), //string - region of destination
+                    'dimension53': b['city'].toLowerCase() + ', ' + b['country'].toLowerCase(), //string - region of destination
                     'dimension57': 'indirect', //string - a type
                     'dimension93': 'hotels', //string - ancillary sub type
                 }]
