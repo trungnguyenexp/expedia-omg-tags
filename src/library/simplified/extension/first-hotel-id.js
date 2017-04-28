d0c0db9aef5d4b9f8246f263aaa535bf40756b1a
yonaik <v-ynaik@expedia.com>
2017-04-28 09:46:13 +0530

    Removing duplicate code from the extension

--- src/library/simplified/extension/first-hotel-id.js
+++ src/library/simplified/extension/first-hotel-id.js
@@ -8,29 +8,3 @@ else if (b['entity.packageSearch.packageSearchParameters.packageHotelSearchParam
     b['hotels.results.results.0.hotelId'] = b['entity.packageSearch.packageSearchParameters.packageHotelSearchParameters.topTenHotelIds'].split('|')[0];
 }
 
-/* christina added to temporarily fix missing hotelId on package pages */
-if (b.entity.packageSearch.packageSearchParameters.packageHotelSearchParameters.topTenHotelIds && utag.isPSR()) {
-    b['hotelId'] = b.entity.packageSearch.packageSearchParameters.packageHotelSearchParameters.topTenHotelIds.split('|');
-} else if (b.entity.packageSearch.packageSearchParameters.packageHotelSearchParameters.selectedHotelId && utag.isPIS()) {
-    b['hotelId'] = b.entity.packageSearch.packageSearchParameters.packageHotelSearchParameters.selectedHotelId
-}
-
-/** old
- if (utag_data['entity.hotels.results.results.0.hotelId'] != undefined)
- {
-     utag_data['hotels.results.results.0.hotelId'] = utag_data['entity.hotels.results.results.0.hotelId'];
- }
- else if (utag_data['entity.packageSearch.packageSearchParameters.packageHotelSearchParameters.topTenHotelIds']
- && utag_data['entity.packageSearch.packageSearchParameters.packageHotelSearchParameters.topTenHotelIds'])
- {
-     utag_data['hotels.results.results.0.hotelId'] = utag_data['entity.packageSearch.packageSearchParameters.packageHotelSearchParameters.topTenHotelIds'].split('|')[0];
- }
-
- /* christina added to temporarily fix missing hotelId on package pages
- if (utag_data.entity.packageSearch.packageSearchParameters.packageHotelSearchParameters.topTenHotelIds && utag.isPSR()) {
-  b['hotelId'] = utag_data.entity.packageSearch.packageSearchParameters.packageHotelSearchParameters.topTenHotelIds.split('|');
-} else if (utag_data.entity.packageSearch.packageSearchParameters.packageHotelSearchParameters.selectedHotelId && utag.isPIS()) {
-  b['hotelId'] = utag_data.entity.packageSearch.packageSearchParameters.packageHotelSearchParameters.selectedHotelId
-}
-
- **/
