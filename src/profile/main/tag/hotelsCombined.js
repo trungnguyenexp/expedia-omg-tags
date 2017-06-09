0eb7e3a72785730efeb5b1696cd2d45c16ae8578
j-ibrahim <v-jibrahim@expedia.com>
2017-06-09 21:38:47 +0530

    Enable Enigma logging for that Hotelscombined (#152)
    https://trello.com/c/SKm4qevH/438-enable-enigma-logging-for-that-hotelscombined

--- src/profile/main/tag/hotelsCombined.js
+++ src/profile/main/tag/hotelsCombined.js
@@ -28,9 +28,9 @@ try{
 	if(b._ccountry)c.push("ClientCountry="+b._ccountry);
       }
       u.img=new Image();u.img.src=u.base_url+c.join(u.qsp_delim);
+      omg.pixel.fireTagPixel({id: id, name: 'hotelscombined', label: 'hotelsCombined', context: {u: u, b: b}});
     }
   }
-  omg.pixel.fireTagPixel({id: id, name: 'hotelscombined', label: 'hotelsCombined', context: {u: u, b: b}});
   try{utag.o[loader].loader.LOAD(id)}catch(e){utag.loader.LOAD(id)}
 })('##UTID##','##UTLOADERID##');
 }catch(e){}
