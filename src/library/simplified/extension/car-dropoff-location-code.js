341775b2b0ab877351d7a47a2eea04ef4dd270a2
jibrahim <v-jibrahim@expedia.com>
2017-09-12 11:44:40 +0530

    Extension updates for Doubleclick flood light tag migration in Car UDP

--- src/library/simplified/extension/car-dropoff-location-code.js
+++ src/library/simplified/extension/car-dropoff-location-code.js
@@ -6,14 +6,6 @@ if(utag.isCarSR() && typeof b["entity.carSearch.searchCriteria.dropOffLocation.l
 else if(utag.isCarCO() && typeof b.entity.tripDetails!= "undefined"){
     b["carDropOffLocationCode"] = b["entity.tripDetails.carInfo.dropOffLocation.locationCode"];
 }
-
-/** old
- if(utag.isCarSR() && typeof utag_data["entity.carSearch.searchCriteria.dropOffLocation.locationCode"] != "undefined"){
-  utag_data["carDropOffLocationCode"] = utag_data["entity.carSearch.searchCriteria.dropOffLocation.locationCode"];
-}else if((utag.isCarPymt() || utag.isCarCO()) && typeof utag_data["entity.checkout.car.dropOffLocation.locationCode"] != "undefined" ){
-  utag_data["carDropOffLocationCode"] = utag_data["entity.checkout.car.dropOffLocation.locationCode"];
+else if(utag.isCarUDP() && typeof b["entity.carDetails.dropOffLocation.locationCode"] !== undefined) {
+	b["carDropOffLocationCode"] = b["entity.carDetails.dropOffLocation.locationCode"];
 }
- else if(utag.isCarCO() && typeof utag_data.entity.tripDetails!= "undefined"){
-  utag_data["carDropOffLocationCode"] = b["entity.tripDetails.carInfo.dropOffLocation.locationCode"];
-}
- **/
