6933d3b4bf9cb0c552db1efedc8f049602ef45fa
Sandeep Kurup <Sandeep Kurup>
2017-09-14 09:02:36 +0530

    DoubleClick Floodlight Tag Migration - Package UDP page

---
+++ src/library/simplified/extension/doubleclick-src-type-cat-PackageUDP.js
@@ -0,0 +1,242 @@
+if (b.siteId && utag.isPRateDetails()) {
+    switch (b.siteId) {
+        case 1:
+            b['doubleclick.src.packageUDP'] = "2588783";
+            b['doubleclick.type.packageUDP'] = "nausc547";
+            b['doubleclick.cat.packageUDP'] = "nausr781";
+            break;
+        case 3:
+            b['doubleclick.src.packageUDP'] = "2588791";
+            b['doubleclick.type.packageUDP'] = "emeau111";
+            b['doubleclick.cat.packageUDP'] = "emeau626";
+            break;
+        case 4:
+            b['doubleclick.src.packageUDP'] = "2588785";
+            b['doubleclick.type.packageUDP'] = "nacan212";
+            b['doubleclick.cat.packageUDP'] = "nacan028";
+            break;
+        case 6:
+            b['doubleclick.src.packageUDP'] = "2589196";
+            b['doubleclick.type.packageUDP'] = "emeag857";
+            b['doubleclick.cat.packageUDP'] = "emeag612";
+            break;
+        case 8:
+            b['doubleclick.src.packageUDP'] = "2589197";
+            b['doubleclick.type.packageUDP'] = "emeai699";
+            b['doubleclick.cat.packageUDP'] = "emeai407";
+            break;
+        case 9:
+            b['doubleclick.src.packageUDP'] = "2589198";
+            b['doubleclick.type.packageUDP'] = "emeas940";
+            b['doubleclick.cat.packageUDP'] = "emeas012";
+            break;
+        case 11:
+            b['doubleclick.src.packageUDP'] = "2589205";
+            b['doubleclick.type.packageUDP'] = "emean493";
+            b['doubleclick.cat.packageUDP'] = "emean928";
+            break;
+        case 12:
+            b['doubleclick.src.packageUDP'] = "2589193";
+            b['doubleclick.type.packageUDP'] = "namex113";
+            b['doubleclick.cat.packageUDP'] = "namex272";
+            break;
+        case 14:
+            b['doubleclick.src.packageUDP'] = "2945316";
+            b['doubleclick.type.packageUDP'] = "apacs971";
+            b['doubleclick.cat.packageUDP'] = "apacs916";
+            break;
+        case 15:
+            b['doubleclick.src.packageUDP'] = "2997500";
+            b['doubleclick.type.packageUDP'] = "apacm103";
+            b['doubleclick.cat.packageUDP'] = "apacm665";
+            break;
+        case 16:
+            b['doubleclick.src.packageUDP'] = "2997587";
+            b['doubleclick.type.packageUDP'] = "apack568";
+            b['doubleclick.cat.packageUDP'] = "apack914";
+            break;
+        case 17:
+            b['doubleclick.src.packageUDP'] = "2997550";
+            b['doubleclick.type.packageUDP'] = "apact438";
+            b['doubleclick.cat.packageUDP'] = "apact188";
+            break;
+        case 18:
+            b['doubleclick.src.packageUDP'] = "3012675";
+            b['doubleclick.type.packageUDP'] = "apach256";
+            b['doubleclick.cat.packageUDP'] = "apact188";
+            break;
+        case 20:
+            b['doubleclick.src.packageUDP'] = "2589195";
+            b['doubleclick.type.packageUDP'] = "emeaf419";
+            b['doubleclick.cat.packageUDP'] = "emeaf862";
+            break;
+        case 25:
+            b['doubleclick.src.packageUDP'] = "2589206";
+            b['doubleclick.type.packageUDP'] = "apaca499";
+            b['doubleclick.cat.packageUDP'] = "apaca758";
+            break;
+        case 27:
+            b['doubleclick.src.packageUDP'] = "2589208";
+            b['doubleclick.type.packageUDP'] = "apaci060";
+            b['doubleclick.cat.packageUDP'] = "apaci469";
+            break;
+        case 28:
+            b['doubleclick.src.packageUDP'] = "2589209";
+            b['doubleclick.type.packageUDP'] = "apacj240";
+            b['doubleclick.cat.packageUDP'] = "apacj153";
+            break;
+        case 29:
+            b['doubleclick.src.packageUDP'] = "2589207";
+            b['doubleclick.type.packageUDP'] = "apacn043";
+            b['doubleclick.cat.packageUDP'] = "apacn584";
+            break;
+        case 61:
+            b['doubleclick.src.packageUDP'] = "3193419";
+            b['doubleclick.type.packageUDP'] = "apaci419";
+            b['doubleclick.cat.packageUDP'] = "apaci850";
+            break;
+        case 62:
+            b['doubleclick.src.packageUDP'] = "2997604";
+            b['doubleclick.type.packageUDP'] = "apact361";
+            b['doubleclick.cat.packageUDP'] = "apact911";
+            break;
+        case 63:
+            b['doubleclick.src.packageUDP'] = "2589199";
+            b['doubleclick.type.packageUDP'] = "emeai191";
+            b['doubleclick.cat.packageUDP'] = "emeai387";
+            break;
+        case 64:
+            b['doubleclick.src.packageUDP'] = "2589204";
+            b['doubleclick.type.packageUDP'] = "emeab264";
+            b['doubleclick.cat.packageUDP'] = "emeab848";
+            break;
+        case 65:
+            b['doubleclick.src.packageUDP'] = "2589201";
+            b['doubleclick.type.packageUDP'] = "emeas325";
+            b['doubleclick.cat.packageUDP'] = "emeas660";
+            break;
+        case 66:
+            b['doubleclick.src.packageUDP'] = "2589200";
+            b['doubleclick.type.packageUDP'] = "emean433";
+            b['doubleclick.cat.packageUDP'] = "emean640";
+            break;
+        case 67:
+            b['doubleclick.src.packageUDP'] = "2589202";
+            b['doubleclick.type.packageUDP'] = "emead466";
+            b['doubleclick.cat.packageUDP'] = "emead839";
+            break;
+        case 68:
+            b['doubleclick.src.packageUDP'] = "2997741";
+            b['doubleclick.type.packageUDP'] = "apacp656";
+            b['doubleclick.cat.packageUDP'] = "apacp184";
+            break;
+        case 69:
+            b['doubleclick.src.packageUDP'] = "3224677";
+            b['doubleclick.type.packageUDP'] = "latam050";
+            b['doubleclick.cat.packageUDP'] = "latam372";
+            break;
+        case 70:
+            b['doubleclick.src.packageUDP'] = "3291249";
+            b['doubleclick.type.packageUDP'] = "latam881";
+            b['doubleclick.cat.packageUDP'] = "latam946";
+            break;
+        case 71:
+            b['doubleclick.src.packageUDP'] = "3291279";
+            b['doubleclick.type.packageUDP'] = "apacv307";
+            b['doubleclick.cat.packageUDP'] = "apacv560";
+            break;
+        case 72:
+            b['doubleclick.src.packageUDP'] = "2589196";
+            b['doubleclick.type.packageUDP'] = "emeag857";
+            b['doubleclick.cat.packageUDP'] = "emeag612";
+            break;
+        case 73:
+            b['doubleclick.src.packageUDP'] = "3847494";
+            b['doubleclick.type.packageUDP'] = "emeaf270";
+            b['doubleclick.cat.packageUDP'] = "emeaf636";
+            break;
+        case 74:
+            b['doubleclick.src.packageUDP'] = "2588791";
+            b['doubleclick.type.packageUDP'] = "emeau111";
+            b['doubleclick.cat.packageUDP'] = "emeau626";
+            break;
+        case 75:
+            b['doubleclick.src.packageUDP'] = "3012675";
+            b['doubleclick.type.packageUDP'] = "apach256";
+            b['doubleclick.cat.packageUDP'] = "apach653";
+            break;
+        case 1146:
+            b['doubleclick.src.packageUDP'] = "3645995";
+            b['doubleclick.type.packageUDP'] = "nausa900";
+            b['doubleclick.cat.packageUDP'] = "nausr631";
+            break;
+        case 1170:
+            b['doubleclick.src.packageUDP'] = "2588783";
+            b['doubleclick.type.packageUDP'] = "nausc547";
+            b['doubleclick.cat.packageUDP'] = "nausr781";
+            break;
+        case 30031:
+            b['doubleclick.src.packageUDP'] = "2588783";
+            b['doubleclick.type.packageUDP'] = "nausc547";
+            b['doubleclick.cat.packageUDP'] = "nausr781";
+            break;
+        case 70125:
+            b['doubleclick.src.packageUDP'] = "4598511";
+            b['doubleclick.type.packageUDP'] = "apaca0";
+            b['doubleclick.cat.packageUDP'] = "apaca001";
+            break;
+        case 70129:
+            b['doubleclick.src.packageUDP'] = "4600929";
+            b['doubleclick.type.packageUDP'] = "apacn00";
+            b['doubleclick.cat.packageUDP'] = "apacn001";
+            break;
+        case 70150:
+            b['doubleclick.src.packageUDP'] = "4693435";
+            b['doubleclick.type.packageUDP'] = "apaca00";
+            b['doubleclick.cat.packageUDP'] = "apaca000";
+            break;
+        case 70151:
+            b['doubleclick.src.packageUDP'] = "4692458";
+            b['doubleclick.type.packageUDP'] = "apacn00";
+            b['doubleclick.cat.packageUDP'] = "apacn000";
+            break;
+        case 70201:
+            b['doubleclick.src.packageUDP'] = "5179340";
+            b['doubleclick.type.packageUDP'] = "nauso00";
+            b['doubleclick.cat.packageUDP'] = "orb03";
+            break;
+        case 70209:
+            b['doubleclick.src.packageUDP'] = "5179340";
+            b['doubleclick.type.packageUDP'] = "nauso00";
+            b['doubleclick.cat.packageUDP'] = "orb03";
+            break;
+        case 70301:
+            b['doubleclick.src.packageUDP'] = "5192003";
+            b['doubleclick.type.packageUDP'] = "nausc0";
+            b['doubleclick.cat.packageUDP'] = "ctix04";
+            break;
+        case 80001:
+            b['doubleclick.src.packageUDP'] = "5439637";
+            b['doubleclick.type.packageUDP'] = "natra00";
+            b['doubleclick.cat.packageUDP'] = "natra00";
+            break;
+        case 3140309:
+            b['doubleclick.src.packageUDP'] = "2589201";
+            b['doubleclick.type.packageUDP'] = "emeas325";
+            b['doubleclick.cat.packageUDP'] = "emeas660";
+            break;
+        case 3920336:
+            b['doubleclick.src.packageUDP'] = "2589202";
+            b['doubleclick.type.packageUDP'] = "emead466";
+            b['doubleclick.cat.packageUDP'] = "emead839";
+            break;
+        case 10122006:
+            b['doubleclick.src.packageUDP'] = "2589203";
+            b['doubleclick.type.packageUDP'] = "emeaa616";
+            b['doubleclick.cat.packageUDP'] = "emeaa963";
+            break;
+
+        default:
+            break;
+    }
+}
