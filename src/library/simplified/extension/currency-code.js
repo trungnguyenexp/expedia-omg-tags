e8344f08710e00b9081f871ab667174e8fdf0b2f
Trung Nguyen <trunguyen@BELC02R3FSYG8WN.sea.corp.expecn.com>
2017-10-16 15:22:32 -0700

    clean up currency-code ext, and remove dead code

--- src/library/simplified/extension/currency-code.js
+++ src/library/simplified/extension/currency-code.js
@@ -1,20 +1,14 @@
 //Hotel path
-if(utag.isHSR()){
-    b["currencyCode"] = b["entity.hotels.resultsSummary.summaryPrice.currency"] ? b["entity.hotels.resultsSummary.summaryPrice.currency"] : "";
+if((utag.isHIS() || utag.isPIS()) && typeof b["entity.hotels.listOfHotels.0.mostEconomicalRoomAvailable.averagePrice.currency"] !== "undefined"){
+    b["currencyCode"] = b["entity.hotels.listOfHotels.0.mostEconomicalRoomAvailable.averagePrice.currency"];
 }
-else if(utag.isHIS() || utag.isPIS()){
-    b["currencyCode"] = b["entity.hotels.listOfHotels.0.mostEconomicalRoomAvailable.averagePrice.currency"] ? b["entity.hotels.listOfHotels.0.mostEconomicalRoomAvailable.averagePrice.currency"] : "";
-}
-if(utag.isHSR() && b["entity.hotels.resultsSummary.summaryPrice.currency"] != undefined){
+else if(utag.isHSR() && typeof b["entity.hotels.resultsSummary.summaryPrice.currency"] !== "undefined"){
     b["currencyCode"] = b["entity.hotels.resultsSummary.summaryPrice.currency"];
 }
 
 //Package
 else if ((utag.isPSR()  || utag.isPSR_F_Responsive() || utag.isPIS_HotelCar()) && typeof b["entity.packageSearch.results.offers.0.packagePrice.packageTotalPrice.currency"] !== "undefined"){
     b["currencyCode"] = b["entity.packageSearch.results.offers.0.packagePrice.packageTotalPrice.currency"];
-}
-else if (utag.isPRateDetails() && typeof b['entity.tripDetails.totalPrice.currency'] !== "undefined") {
-    b['currencyCode'] = b['entity.tripDetails.totalPrice.currency'];
 }
 
 //Flight path
@@ -24,11 +18,8 @@ else if (utag.isFSR() && b["entity.flightSearch.searchResults.cheapest.price.cur
 else if(utag.isFRateDetails() && b["entity.tripDetails.flightOffer.price.currency"] != undefined){
     b["currencyCode"] = b["entity.tripDetails.flightOffer.price.currency"];
 }
-else if(utag.isFPymt()){
-    b["currencyCode"] = b["entity.checkout.cartTotal.currency"] ? b["entity.checkout.cartTotal.currency"] : "";
-}
-else if(utag.isFCO()){
-    b["currencyCode"] = b["entity.checkout.flightOffer.totalPrice.currency"] ? b["entity.checkout.flightOffer.totalPrice.currency"] : "";
+else if(utag.isFCO() && typeof b["entity.checkout.flightOffer.totalPrice.currency"] !== "undefined"){
+    b["currencyCode"] = b["entity.checkout.flightOffer.totalPrice.currency"];
 }
 
 //Car path
@@ -38,15 +29,14 @@ else if(utag.isCarSR() && b["entity.carSearch.lowestOfferDailyPrice.currency"] !
 else if (utag.isPCarSearch() && typeof b["entity.carSearch.lowestOfferTotalPrice.currency"] !== "undefined") {
     b["currencyCode"] = b["entity.carSearch.lowestOfferTotalPrice.currency"];
 }
+
+//Common package
 else if (utag.isPSR_HC() || utag.isPSR_FC() || utag.isPSR_FH_Responsive() || utag.isPIS()) {
     if (typeof b.entity.packageSearch.results !== "undefined" && typeof b.entity.packageSearch.results.offers !== "undefined") {
         b["currencyCode"] = b["entity.packageSearch.results.offers.0.packagePrice.packageTotalPrice.currency"] ? b["entity.packageSearch.results.offers.0.packagePrice.packageTotalPrice.currency"] : "";
     } else if (typeof b["entity.multiItemSearch.multiItemSearchResults.offers.0.packagePrice.packageTotalPrice.currency"] !== "undefined") {
         b["currencyCode"] = b["entity.multiItemSearch.multiItemSearchResults.offers.0.packagePrice.packageTotalPrice.currency"];
     }
-}
-else if(utag.isCarSR() && b["entity.carSearch.lowestOfferDailyPrice.currency"] != undefined){
-    b["currencyCode"] = b["entity.carSearch.lowestOfferDailyPrice.currency"];
 }
 else if(utag.isCarUDP() && b["entity.carDetails.totalPrice.currency"] !== undefined) {
 	b["currencyCode"] = b["entity.carDetails.totalPrice.currency"];
@@ -67,31 +57,18 @@ else if (utag.isRailRateDetails() && b['entity.railSearch.railDetail.railLegs.0.
 }
 
 //Common data element
-else if((utag.isCarPymt() || utag.isCarCO() || utag.isHCO() || utag.isHPymt()
-    || utag.isCruisePymt() || utag.isCruiseCO() || utag.isLXCO() || utag.isPCO() || utag.isPCO_Mobile() || utag.isMCO() || utag.isRCO()) && typeof b["entity.checkout.cartTotal.currency"] != "undefined"){
+else if((utag.isCarPymt() || utag.isCarCO() || utag.isHCO() || utag.isHPymt() || utag.isCruisePymt() || utag.isCruiseCO()
+    || utag.isLXCO() || utag.isPCO() || utag.isPCO_Mobile() || utag.isMCO() || utag.isRCO() || utag.isFPymt()
+    || utag.isLXCO() || utag.isLXGT_CO()) && typeof b["entity.checkout.cartTotal.currency"] != "undefined"){
     b["currencyCode"] = b["entity.checkout.cartTotal.currency"];
 }
-else if(utag.isLXCO() && b.entity.checkout.cartTotal.currency){
-    b["currencyCode"] = b.entity.checkout.cartTotal.currency;
-}
-else if(utag.isCarCO() && typeof b["entity.tripDetails.totalPrice"] != "undefined"){
+else if((utag.isPRateDetails() || utag.isCarCO() || utag.isItinPage()) && typeof b["entity.tripDetails.totalPrice.currency"] != "undefined"){
     b['currencyCode'] = b["entity.tripDetails.totalPrice.currency"] ? b["entity.tripDetails.totalPrice.currency"] : "";
-}
-else if(utag.isItinPage() && typeof b["entity.tripDetails.totalPrice"] != "undefined"){
-    b['currencyCode'] = b["entity.tripDetails.totalPrice.currency"] ? b["entity.tripDetails.totalPrice.currency"] : "";
-}
-else if(utag.is3pp()){
-    b['currencyCode'] = b["entity.checkout.currency"];
 }
 else if(utag.isPCF() && typeof b['entity.packageSearch.results.offers.0.packagePrice.packageTotalPrice.currency'] !== undefined) {
     b['currencyCode'] = b['entity.packageSearch.results.offers.0.packagePrice.packageTotalPrice.currency'];
 }
-else if(utag.isLXGT_CO() && b.entity.checkout.cartTotal.currency){
-    b['currencyCode'] = b.entity.checkout.cartTotal.currency;
-}
-
-//All confirmation pages in cluding Slim confirmation pages.
-if(b.pageInfo.pageName && b.pageInfo.pageName.indexOf("Checkout.Confirmation") > -1 && b.entity.checkout.currency){
+else if((utag.is3pp() || b.pageInfo.pageName && b.pageInfo.pageName.indexOf("Checkout.Confirmation") > -1 ) && b.entity.checkout.currency){
     b['currencyCode'] = b.entity.checkout.currency;
 }
 
--- src/library/simplified/extension/trl.js
+++ src/library/simplified/extension/trl.js
@@ -7,13 +7,3 @@ else if (b['entity.checkout.trl'] == "" || b['entity.checkout.trl'] == undefined
     b['checkout.trl'] = b['entity.checkout.orderNumber'];
     b['checkoutTRL'] = b['entity.checkout.orderNumber'];
 }
-
-/** old
- if (utag_data['entity.checkout.trl'] != undefined) {
-  utag_data['checkout.trl'] = utag_data['entity.checkout.trl'];
-}
- else if (utag_data['entity.checkout.trl'] == "" || utag_data['entity.checkout.trl'] == undefined) {
-  utag_data['entity.checkout.trl'] = utag_data['entity.checkout.orderNumber'];
-  utag_data['checkout.trl'] = utag_data['entity.checkout.orderNumber'];
-}
- **/
